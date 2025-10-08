ec7d68e8838bf87e35f115e245c2edac203d3054
ram58059 <kram58059@gmail.com>
2025-10-08 22:23:25 +0530

    add files

--- geeks_site/geeks/views.py
+++ geeks_site/geeks/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def greet_hello(request):
+    return HttpResponse("Hello, welcome to Geeks World!")
--- geeks_site/geeks_site/urls.py
+++ geeks_site/geeks_site/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from geeks.views import greet_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('geeks.urls')),
+    # path('greet/', greet_hello),
 ]
