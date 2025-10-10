7d41b6fe9d94f1e307798585fe5ab13e0e0e462e
ram58059 <kram58059@gmail.com>
2025-10-10 22:20:28 +0530

    dummy

--- geeks_site/geeks/urls.py
+++ geeks_site/geeks/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.create_view, name='create_view'),
     path('list/', views.list_view, name='list_view'),
-    path('<id>', views.detail_view, name='detail_view'),  # Optional: Detail view for individual entries
-    path('<id>/update', views.update_view, name='update_view'),  # Optional: Update view for individual entries
-    path('<id>/delete', views.delete_view, name='delete_view'),  # Optional: Delete view for individual entries
+    path('<id>', views.detail_view, name='detail_view'), 
+    path('<id>/update', views.update_view, name='update_view'),  
+    path('<id>/delete', views.delete_view, name='delete_view'),  
 ]
