3f73458fa1e17e5fe7b06f9a8e609ff0938cbaf9
Ramkumar <80213528+ram58059@users.noreply.github.com>
2025-10-14 21:30:43 +0530

    Update models.py

--- geeks_site/geeks/models.py
+++ geeks_site/geeks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here
+# Create your models here.
 class Geekmodels(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
